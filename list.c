41923db79466138c59759e0ed4799e14668bddc2
Your Name <you@example.com>
2025-03-26 21:30:15 -0300

    new try

--- list.c
+++ list.c
@@ -52,7 +52,7 @@ void * nextList(List * list) {
 
 void * lastList(List * list) {
     //usar ultimo dato
-    if(list->tail == NULL) return NULL;
+    if(list->current->prev == NULL) return NULL;
     list->current = list->tail;
     return list->tail->data;//el dato queda como el ultimo de la lista
 }
